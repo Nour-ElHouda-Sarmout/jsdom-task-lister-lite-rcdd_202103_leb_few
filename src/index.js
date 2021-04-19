88e26705096af0a104f998a09d10066838c54694
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-04-19 00:36:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit",function(event) {
          document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
          event.preventDefault();
-  });
-
-
+}, false);)
 });
