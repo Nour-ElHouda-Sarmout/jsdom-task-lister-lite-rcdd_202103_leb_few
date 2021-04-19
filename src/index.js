ac817df04faf035579512193e01da78f2f95bf05
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-04-19 00:34:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit",function(event) {
+         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+         event.preventDefault();
+  });
+
+
 });
